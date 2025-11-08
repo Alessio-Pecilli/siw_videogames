71346e98d812ed6a4c7d971532769658eb8dacf9
Alessio <alessiopecilli@gmail.com>
2025-11-08 11:25:38 +0100

    Fix: make VideogamesApplicationTests load application context with @SpringBootTest(classes=...) and @ActiveProfiles(test)

--- src/test/java/uniroma3/siw/videogames/videogames/VideogamesApplicationTests.java
+++ src/test/java/uniroma3/siw/videogames/videogames/VideogamesApplicationTests.java
@@ -2,12 +2,16 @@ package uniroma3.siw.videogames.videogames;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(
+	classes = uniroma3.siw.videogames.VideogamesApplication.class
+)
+@ActiveProfiles("test")
 class VideogamesApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Verifica che il contesto Spring si avvii correttamente
 	}
-
 }
